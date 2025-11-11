0c99246fb59f21ed061ce1fd9afab1fc466a8e5d
ashishmohanty10 <ashishmohaty10@gmail.com>
2025-11-12 02:34:25 +0530

    feat: integrate hero video into hero section

--- src/components/landing-page/hero.tsx
+++ src/components/landing-page/hero.tsx
@@ -1,7 +1,6 @@
 import { Container } from '@/components/common/container'
 import { HeroSubTitle, HeroTitle, Tiles } from '@/components/common/titles'
-import { URLShortenerHome } from '@/components/url/url-shortener-home'
-import Image from 'next/image'
+import { AuthButton } from '../common/auth-button'
 
 export const Hero = () => {
   return (
@@ -18,22 +17,25 @@ export const Hero = () => {
               <br className="hidden lg:block" /> Powerful analytics and custom domains{' '}
               <br className="md:hidden" /> Built for Creators, Businesses, and Developers.
             </HeroSubTitle>
+
+            <div className='flex items-center justify-center mt-5'>
+              <AuthButton hideSignin signupText='Get Started Now!' />
+            </div>
           </div>
 
-          <URLShortenerHome />
         </div>
 
-        <div className="w-full h-full flex justify-center mt-10">
-          <Image
-            src="/demo-hero-img.webp"
-            alt="Hero Image"
-            priority
-            quality={100}
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 90vw, 1200px"
-            width={1000}
-            height={1000}
-            className="blur-sm"
-          />
+        <div className="w-full h-full flex justify-center rounded-sm">
+          <video preload="auto" className='max-w-sm md:max-w-3xl lg:max-w-6xl w-full rounded-sm' autoPlay muted loop playsInline >
+            <source src="/hero-video.mp4" type="video/mp4" />
+            <track
+              src="/path/to/captions.vtt"
+              kind="subtitles"
+              srcLang="en"
+              label="English"
+            />
+            Your browser does not support the video tag.
+          </video>
         </div>
       </Container>
     </div>

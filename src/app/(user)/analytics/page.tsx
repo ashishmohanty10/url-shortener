307e07528f9134cadb00058fcfe2e726beadc988
ashishmohanty10 <ashishmohaty10@gmail.com>
2025-11-26 13:55:58 +0530

    feat: enhance analytics page to fetch and display detailed data

--- src/app/(user)/analytics/page.tsx
+++ src/app/(user)/analytics/page.tsx
@@ -1,14 +1,17 @@
 import { Analytics } from '@/components/analytics/analytics'
 import { PageHeader } from '@/components/common/titles'
+import { getUrlAnalyticsAction } from '@/server/get-analytics-action'
 import { requireAuth } from '@/utils/auth-guard'
+import { AnalyticsProps } from '@/utils/types'
 
 export default async function AnalyticsPage() {
   await requireAuth()
+  const data: AnalyticsProps = await getUrlAnalyticsAction()
 
   return (
     <div className="lg:h-[94vh] lg:overflow-y-auto w-full">
       <PageHeader>Analytics</PageHeader>
-      <Analytics />
+      <Analytics data={data} />
     </div>
   )
 }
--- src/components/analytics/analytics.tsx
+++ src/components/analytics/analytics.tsx
@@ -1,27 +1,18 @@
-import { DailyClick } from '@/utils/types'
+import { AnalyticsProps } from '@/utils/types'
 import { StatCard } from '../common/profile-stat-card'
 import { AnalyticsChart } from './analytics-chart'
 import { ShortLinkAnalyticsCard } from './short-link-analytics-card'
-import { getUrlAnalyticsAction } from '@/server/get-analytics-action'
 import { ShortLinkInsightsPanel } from './short-link-insight-panel'
 
-interface AnalyticsProps {
-  count: number
-  totalClicks: number
-  daily: DailyClick[]
-}
-
-export async function Analytics() {
-  const { count, totalClicks, daily }: AnalyticsProps = await getUrlAnalyticsAction()
-
+export async function Analytics({ data }: { data: AnalyticsProps }) {
   return (
     <div className="mt-4 w-full">
       <div className="grid grid-cols-2 items-center gap-x-2 lg:gap-x-5 mb-5 w-full">
-        <StatCard label="Total Shortened URLs" value={count} />
-        <StatCard label="Total Clicks" value={totalClicks} />
+        <StatCard label="Total Shortened URLs" value={data.count} />
+        <StatCard label="Total Clicks" value={data.totalClicks} />
       </div>
 
-      <AnalyticsChart data={daily} />
+      <AnalyticsChart data={data.daily} />
       <div className="grid grid-cols-1 lg:grid-cols-2 items-center justify-center lg:gap-x-5 mt-5 w-full space-y-5 lg:space-y-0">
         <ShortLinkAnalyticsCard />
         <ShortLinkInsightsPanel />

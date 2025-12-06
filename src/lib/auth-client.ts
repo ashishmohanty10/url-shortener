3161fe48a087a243a4a1eed7e71c93ec4b8cfc81
ashishmohanty10 <ashishmohaty10@gmail.com>
2025-12-06 21:42:57 +0530

    fix: failed to fetch dynamically imported module: node:async_hooks

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,7 @@
 import { createAuthClient } from 'better-auth/react'
 import { adminClient, inferAdditionalFields } from 'better-auth/client/plugins'
-import { nextCookies } from 'better-auth/next-js'
 import { auth } from '@/lib/auth'
 
 export const authClient = createAuthClient({
-  plugins: [adminClient(), nextCookies(), inferAdditionalFields<typeof auth>()],
+  plugins: [adminClient(), inferAdditionalFields<typeof auth>()],
 })
